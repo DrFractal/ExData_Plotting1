1390414a2dd0dd87b45568cf59873e4c3017f00a
DrFractal <KnightOfChess@gmail.com>
2015-06-04 02:13:08 -0400

    Plot 4 Working
    Plot 4 is working

---
+++ plot4.R
@@ -0,0 +1,44 @@
+# Exploratory Data Analysis
+# Project 1
+# Plot 4 of 4
+
+path2csv <- "D:/R/Coursera/Exploratory Data Analysis/Project 1/ExData_Plotting1/household_power_consumption.txt"
+#classes <- c("Date", character, numeric, numeric, numeric, numeric, numeric, numeric, numeric)
+colTypes <- c(character, character, 
+              numeric, numeric, numeric, numeric, numeric, numeric, numeric)
+mydf <- read.csv(path2csv, sep = ";", #colClasses = colTypes, 
+                 na.strings = "?", blank.lines.skip = TRUE,
+                 stringsAsFactors = FALSE)
+
+library(dplyr)
+plot1df <- tbl_df(mydf)
+rm("mydf")  ## clean up
+
+myRows <- filter(plot1df, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Combine the date and time vectors into one date object vector
+dateTime <- strptime(paste(myRows$Date, " ", myRows$Time), "%d/%m/%Y %X")
+
+png("plot4.png")
+attach(mtcars)
+par(mfrow = c(2,2)) # 2x2 matrix of graphs
+plot(dateTime, as.numeric(myRows$Global_active_power), type = 'l', ylab = 'Global Active Power',
+     xlab = '')
+
+plot(dateTime, as.numeric(myRows$Voltage), type = 'l', ylab = 'Voltage',
+     xlab = 'datetime')
+
+plot(dateTime, as.numeric(myRows$Sub_metering_1), type = 'l', ylab = 'Energy sub metering',
+     xlab = '')
+lines(dateTime, as.numeric(myRows$Sub_metering_2), col = 'red')
+lines(dateTime, as.numeric(myRows$Sub_metering_3), col = 'blue')
+legend('topright', c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), 
+       lty = 1, # display lines for the symbols
+       lwd = 2, # the thickness of the lines
+       col = c('black', 'red', 'blue'),  # colors for the lines
+       bty = 'n')  # turn off the legend border
+
+plot(dateTime, as.numeric(myRows$Global_reactive_power), type = 'l', ylab = 'Global_reactive_power',
+     xlab = 'datetime')
+
+dev.off()
