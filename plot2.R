a9ce85be9e9437ddb694b690f613c5c4dc5ebf2d
DrFractal <KnightOfChess@gmail.com>
2015-06-04 01:18:21 -0400

    Plot 2 works
    Plot 2 works
    Minor change to Plot 1 to use as.numeric

--- plot2.R
+++ plot2.R
@@ -1,16 +1,14 @@
 # Exploratory Data Analysis
 # Project 1
-# Plot 1 of 4
+# Plot 2 of 4
 
 path2csv <- "D:/R/Coursera/Exploratory Data Analysis/Project 1/ExData_Plotting1/household_power_consumption.txt"
 #classes <- c("Date", character, numeric, numeric, numeric, numeric, numeric, numeric, numeric)
 colTypes <- c(character, character, 
-             numeric, numeric, numeric, numeric, numeric, numeric, numeric)
+              numeric, numeric, numeric, numeric, numeric, numeric, numeric)
 mydf <- read.csv(path2csv, sep = ";", #colClasses = colTypes, 
                  na.strings = "?", blank.lines.skip = TRUE,
                  stringsAsFactors = FALSE)
-#dim(mydf)
-#head(mydf)
 
 library(dplyr)
 plot1df <- tbl_df(mydf)
@@ -18,7 +16,16 @@ rm("mydf")  ## clean up
 
 myRows <- filter(plot1df, Date == "1/2/2007" | Date == "2/2/2007")
 
-png("plot1.png") #, width = 4, height = 4, units = "in", res = 300)
-hist(as.numeric(myRows$Global_active_power), freq = TRUE, breaks = 12, 
-     col = 'red', xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power')
+# strptime(myRows$Time, "%X")
+# as.Date(myRows$Date, "%d/%m/%Y")
+
+# dd <- as.Date(paste(myRows$Date, " ", myRows$Time), "%d/%m/%Y %X")
+# d2 <- strftime(dd)
+
+# Combine the date and time vectors into one date object vector
+dateTime <- strptime(paste(myRows$Date, " ", myRows$Time), "%d/%m/%Y %X")
+
+png("plot2.png")
+plot(dateTime, as.numeric(myRows$Global_active_power), type = 'l', ylab = 'Global Active Power (kilowatts)',
+     xlab = '')
 dev.off()
